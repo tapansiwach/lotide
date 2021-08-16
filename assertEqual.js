4a50baae16f5badc66e9ae3201144939b89576e9
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-16 17:28:38 -0400

    add functionality to assert equality

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274c Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual('lighthouse labs', 'bootcamp');
+assertEqual(1, 1);
+assertEqual('lighthouse labs', 'lighthouse labs');
+assertEqual(1, 2);
