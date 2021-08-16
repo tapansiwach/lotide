d06d21962667bc919267cc0fd3824f706d6b8893
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-16 18:05:01 -0400

    Add functionality to get the tail of an array

---
+++ tail.js
@@ -0,0 +1,23 @@
+// IMPORT FUNCTIONALITY FROM PARALLEL FILE
+const assertEqual = require("./assertEqual");
+
+// FUNCTION IMPLEMENTATION
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+// TEST CODE
+// Test Case 1: Check the returned array elements
+const result1 = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result1.length, 2); // ensure we get back two elements
+assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: Check for empty array
+const result2 = tail([]);
+assertEqual(result2[0], undefined); // ensure that we don't get anything back
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
