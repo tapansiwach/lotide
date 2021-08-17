6131def6b714aa88a8f86cd5dc8348f4582ae3a2
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-17 13:54:27 -0400

    add functionality to check equality of (shallow) arrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = require("./assertEqual");
+
+const eqArrays = function (firstArr, secondArr) {
+  if (firstArr.length !== secondArr.length) {
+    return false;
+  }
+  for (let j = 0; j < firstArr.length; j++) {
+    if (firstArr[j] !== secondArr[j]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+// CODE TESTS
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
