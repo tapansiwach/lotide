3a578c0d880369faffe76416c352bda90e531d20
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-20 13:04:43 -0400

    update functinalty to check for equality of nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,34 @@
 const assertEqual = require("./assertEqual");
 
 const eqArrays = function(firstArr, secondArr) {
+  let result = true;
   if (firstArr.length !== secondArr.length) {
-    return false;
+    result = false;
   }
   for (let j = 0; j < firstArr.length; j++) {
-    if (firstArr[j] !== secondArr[j]) {
-      return false;
+    if (Array.isArray(firstArr[j]) && Array.isArray(secondArr[j])) {
+      result = eqArrays(firstArr[j], secondArr[j]);
+    }
+    else if (firstArr[j] !== secondArr[j]) {
+      result = false;
     }
   }
-  return true;
+  return result;
 };
 
 // CODE TESTS
 if (module.parent === null) {
-  console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-  console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
-  console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-  console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+  // test for nested arrays
+  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
 }
 
 // EXPORT FILE
