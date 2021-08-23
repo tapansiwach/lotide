2d63662f3b82eb56498b8e00ef98ac09a4cf3fec
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-23 03:23:39 -0400

    refactor tests for assertEqual and head into separate files

--- head.js
+++ head.js
@@ -6,7 +6,15 @@ const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+// CODE TESTS
+if (module.parent === null) {
+
+  assertEqual(head([5, 6, 7]), 5);
+  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  assertEqual(head([5]), 5);
+  assertEqual(head([]), undefined);
+
+}
+
+// EXPORT FUNCTIONALITY
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require("../assertEqual")
+
+assertEqual('lighthouse labs', 'bootcamp');
+assertEqual(1, 1);
+assertEqual('lighthouse labs', 'lighthouse labs');
+assertEqual(1, 2);
