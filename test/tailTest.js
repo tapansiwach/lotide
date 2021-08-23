8f8a02883338727608653183519536d238ec8ea3
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-23 04:43:55 -0400

    convert tailTest to use M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,25 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
+
+// const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
-// Test Case 1: Check the returned array elements
-const result1 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result1.length, 2); // ensure we get back two elements
-assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
 
-// Test Case 2: Check for empty array
-const result2 = tail([]);
-assertEqual(result2[0], undefined); // ensure that we don't get anything back
+  it("returns last two elements of an array", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+    assert.strictEqual(result[0], "Lighthouse");
+    assert.strictEqual(result[1], "Labs");
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("return an empty array when given an empty array", () => {
+    assert.strictEqual(tail([])[0], undefined);
+  });
 
+  it("does not modify the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+});
