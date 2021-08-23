29d06b1f3ba5db106668a02e723bd8a86efa321a
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-23 05:31:20 -0400

    export all functions in the library

---
+++ index.js
@@ -0,0 +1,40 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const assertEqual = require("./assertEqual");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const letterPosition = require("./letterPosition");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+
+
+module.exports = {
+
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPosition: letterPosition,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+
+}
